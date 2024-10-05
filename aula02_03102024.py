81bc541e92697001b279f3839a233d07f4695d95
Tete30 <stephanie.oliveira@aluno.senac.rio>
2024-10-04 21:42:53 -0300

    Add files via upload

---
+++ aula02_03102024.py
@@ -0,0 +1,20 @@
+# temperatura=40
+
+ 
+# if temperatura<37.5:
+#     print("você não está com febre.")
+# elif temperatura==37.5:
+#     print("vocÊestá febriu.")
+# else:
+#     print("você está febriu.")
+
+brigadeiro=100
+
+
+if brigadeiro<99:
+    print("não bateu a meta.")
+elif brigadeiro==100:
+    print("bateu a meta.")
+else:
+    print("meta mais que batida")
+
